0239238dfb887f1a1777a6f47c8ba09243534995
Kim YoungJin <smilup2244@gmail.com>
2024-04-25 02:38:14 +0900

    Modified README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bugshot roulette environment
 
 [Bugshot roulette](https://store.steampowered.com/app/2835570/Buckshot_Roulette/) 게임의 룰을 모방한 환경입니다.
+
+## 해보기
+
+`game.py`로 AI와 싸워볼 수 있습니다. 룰은 [위키](https://en.wikipedia.org/wiki/Buckshot_Roulette) 참조.
--- game.py
+++ game.py
@@ -25,18 +25,17 @@ def main():
     game_builder = BugShotGameBuilder()
     game = game_builder.build(config=config)
 
-    dispatcher = game_builder.build(config=config).dispatcher
     monte_agent = MonteCarloBugShotGameAgent(
-        num_trials=1000,
-        dispatcher=dispatcher,
-        imagine=CombinationBugShotImagine(max_chambers=100),
-        explorer=RandomBugShotStateExplorer(dispatcher=dispatcher),
+        num_trials=10000,
+        dispatcher=game.dispatcher,
+        imagine=CombinationBugShotImagine(max_chambers=10000),
+        explorer=RandomBugShotStateExplorer(dispatcher=game.dispatcher),
     )
 
     while game.get_winner() is None:
         print_state(game.state)
         if game.get_turn() == BugShotPlayer.PLAYER1:
-            action = input_action(dispatcher.get_available_actions(game.state))
+            action = input_action(game.dispatcher.get_available_actions(game.state))
             print(f'Player action: {action.value}')
         else:
             action = monte_agent.act(observation=game.observe())
@@ -60,12 +59,13 @@ def print_state(state: BugShotState):
     magnifier_result = 'live' if is_magnified_live else 'blank' if is_magnified_blank else 'none'
     is_shotgun_sawed = state.is_shotgun_sawed
     
-    print(f'--------------- Observation ---------------')
+    turn = 'My' if state.turn == BugShotPlayer.PLAYER1 else 'Opponent'
+    print(f'--------------- {turn} Turn ---------------')
     print(f'{"Live shells":<{indent}}: {num_live_shells}')
     print(f'{"Blank shells":<{indent}}: {num_blank_shells}')
     print(f'{"Lifes":<{indent}}: {life_player1} vs {life_player2}')
-    print(f'{"Items player1":<{indent}}: {items_player1}')
-    print(f'{"Items player2":<{indent}}: {items_player2}')
+    print(f'{"My       items":<{indent}}: {items_player1}')
+    print(f'{"Opponent items":<{indent}}: {items_player2}')
     if is_opponent_handcuffed:
         print(f'{"Opponent handcuffed":<{indent}}: True')
     if magnifier_result != 'none':
@@ -74,8 +74,9 @@ def print_state(state: BugShotState):
         print(f'{"Shotgun sawed":<{indent}}: True')
 
 def input_action(valid_actions: list[BugShotAction]):
-    valid_action_values = [x.value for x in valid_actions]
-    print(f'Available actions: {valid_action_values}')
+    print(f'Available actions:')
+    for i, action in enumerate(valid_actions):
+        print(f'  {i + 1}. {action.value[4:].replace("_", " ")}')
     while True:
         try:
             action = input(f'Enter action (1-{len(valid_actions)}): ')
